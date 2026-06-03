0e9bff5dba6b323853ed57454517bff562fbbd31
Liam White <whiteln@ornl.gov>
2026-06-03 08:32:32 -0400

    fix(geometry): measure helical arc length correctly
    Compute ArcSegment length from planar XY radius and sweep, then combine that planar distance with the Z delta. The previous implementation multiplied the sweep by the full 3D center-to-start distance, which treated center Z as part of the radius and still did not produce the true helical path length.
    
    Route arc extrusion calculations in Marlin, FiveAxisMarlin, Mach4, KraussMaffei, and RepRap writers through ArcSegment::length using the writer-provided angle. This keeps extrusion accounting aligned with corrected arc geometry and preserves explicit arc sweeps such as spiral lift.

--- src/gcode/writers/marlin_writer.cpp
+++ src/gcode/writers/marlin_writer.cpp
@@ -11,6 +11,7 @@
 #include "gcode/gcode_meta.h"
 #include "gcode/writers/writer_base.h"
 #include "geometry/point.h"
+#include "geometry/segments/arc.h"
 #include "units/unit.h"
 #include "utilities/constants.h"
 #include "utilities/enums.h"
@@ -547,7 +548,7 @@ QString MarlinWriter::writeArc(const Point& start_point, const Point& end_point,
         else
             current_multiplier = m_sb->setting<double>(PS::Perimeter::kExtrusionMultiplier);
 
-        Distance length = angle() * center_point.distance(start_point);
+        Distance length = ArcSegment(start_point, end_point, center_point, angle, ccw).length();
         Distance width = params->setting<Distance>(SS::kWidth);
         Distance height = params->setting<Distance>(SS::kHeight);
         Distance filament_diameter = m_sb->setting<Distance>(MS::Filament::kDiameter);
