dab254d28ed4d97a13f8d16ad1ff3d21330b617f
Alex Roschli <roschliac@ornl.gov>
2026-07-15 10:19:29 -0400

    Refresh seam guides when slicing vector changes

--- src/widgets/part_widget/part_widget.cpp
+++ src/widgets/part_widget/part_widget.cpp
@@ -178,6 +178,7 @@ void PartWidget::handleModifiedSetting(const QString& setting_key) {
     }
     else if (slicing_settings.contains(setting_key)) {
         m_part_view->updateSlicingSettings(GSM->getGlobal());
+        m_part_view->updateOptimizationSettings(GSM->getGlobal());
     }
     else if (optimization_settings.contains(setting_key)) {
         m_part_view->updateOptimizationSettings(GSM->getGlobal());
