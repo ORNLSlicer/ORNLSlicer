59391039b1e923f061a5d841c61159ea1f48bf45
Alex Roschli <roschliac@ornl.gov>
2026-08-04 20:54:51 -0500

    Add G-Code to S2C importer

---
+++ include/gcode/gcode_settings_importer.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <functional>
+#include <optional>
+
+#include <QString>
+#include <QStringList>
+
+#include "utilities/qt_json_conversion.h"
+
+namespace ORNL {
+
+class GcodeSettingsImporter {
+  public:
+    struct ImportResult {
+        fifojson settings_file;
+        QStringList errors;
+        QStringList warnings;
+        QStringList imported_keys;
+        QStringList defaulted_keys;
+        QStringList prompted_keys;
+        QStringList missing_keys;
+        QStringList unknown_keys;
+    };
+
+    using MissingValueCallback =
+        std::function<std::optional<fifojson>(const QString& key, const fifojson& master_entry)>;
+
+    static ImportResult importFile(const QString& gcode_path, bool use_defaults_for_missing,
+                                   const MissingValueCallback& missing_value_callback = MissingValueCallback());
+
+    static bool validateValue(const QString& key, const fifojson& master_entry, const fifojson& value,
+                              fifojson& normalized_value, QString& error, bool enforce_ranges = true);
+
+    static QString displayName(const fifojson& master_entry);
+    static QStringList settingOptions(const fifojson& master_entry);
+};
+
+} // namespace ORNL
---
+++ include/windows/gcode_to_s2c.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <optional>
+
+#include <QDialog>
+#include <QLabel>
+#include <QLineEdit>
+#include <qcheckbox.h>
+#include <qdialogbuttonbox.h>
+#include <qgridlayout.h>
+#include <qpushbutton.h>
+#include <qtmetamacros.h>
+
+#include "utilities/qt_json_conversion.h"
+
+namespace ORNL {
+
+class GcodeToS2CDialog : public QDialog {
+    Q_OBJECT
+  public:
+    explicit GcodeToS2CDialog(QWidget* parent = nullptr);
+
+    QString outputFilePath() const;
+
+  private slots:
+    void browseGcodeFile();
+    void browseOutputFile();
+    void refreshAcceptState();
+    void accept() override;
+
+  private:
+    void setupUi();
+    void setDefaultOutputPath();
+    std::optional<fifojson> promptForMissingValue(const QString& key, const fifojson& master_entry);
+
+    QGridLayout* m_layout;
+    QLineEdit* m_gcode_edit;
+    QLineEdit* m_output_edit;
+    QPushButton* m_gcode_browse;
+    QPushButton* m_output_browse;
+    QCheckBox* m_use_defaults;
+    QLabel* m_status_label;
+    QDialogButtonBox* m_buttons;
+};
+
+} // namespace ORNL
