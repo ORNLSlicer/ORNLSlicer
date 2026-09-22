a282f973f7c987d71b5c3c1a1b420e25dabf84c9
Jason <157201244+jkwookee@users.noreply.github.com>
2026-09-22 19:15:41 -0400

    fix(visualization_colors_tests): change test values to match colors set in #275

--- tests/visualization_colors_tests.cpp
+++ tests/visualization_colors_tests.cpp
@@ -54,9 +54,9 @@ const std::vector<ExpectedVisualizationColor>& expectedVisualizationColors() {
         {ORNL::VisualizationColors::kTipWipeReverse, "TipWipeReverse", QColor(179, 128, 255, 255)},
         {ORNL::VisualizationColors::kTravel, "Travel", QColor(233, 175, 198, 255)},
         {ORNL::VisualizationColors::kUnknown, "Unknown", QColor(0, 0, 0, 255)},
-        {ORNL::VisualizationColors::kHelicalPerimeter, "HelicalPerimeter", QColor(153, 51, 255, 255)},
-        {ORNL::VisualizationColors::kHelicalInset, "HelicalInset", QColor(178, 102, 255, 255)},
-        {ORNL::VisualizationColors::kHelicalInfill, "HelicalInfill", QColor(102, 0, 204, 255)},
+        {ORNL::VisualizationColors::kHelicalPerimeter, "HelicalPerimeter", QColor(0, 0, 255, 255)},
+        {ORNL::VisualizationColors::kHelicalInset, "HelicalInset", QColor(0, 204, 255, 255)},
+        {ORNL::VisualizationColors::kHelicalInfill, "HelicalInfill", QColor(0, 255, 0, 255)},
     };
 
     return expected;
